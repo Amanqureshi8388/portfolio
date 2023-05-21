8fc311ab281fc4bbc66670819816ef8da606dc74
Aman qureshi <Amanqureshi8388@gmail.com>
2023-05-21 23:17:43 +0530

    aa

--- src/app/components/hero/Hero.jsx
+++ src/app/components/hero/Hero.jsx
@@ -22,7 +22,6 @@ const Hero = () => {
             <p className={'content-writing lg:w-[70%]'}>
                I{`'`} m a <span className="highlight">web developer</span> from Delhi, India. In my spare time I enjoy DIY, gaming and building my own projects. <span className="highlight">My preferred tool are React.js and Next.js</span> .  I create successful websites that are fast, easy to use, and built with best practices.
             </p>
-            
             <div className={`${style.techStack} flex items-center gap-5 md:gap-10 my-[3rem] lg:my-[5rem] text-lg md:text-2xl`}>
               <p>Tech Stack</p>
               <div className='text-white'>|</div>
